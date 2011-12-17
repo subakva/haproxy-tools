5ddc6e673bfc11b2ce36ebed1e5174f26cc29cb4
Jason Wadsworth <jdwadsworth@gmail.com>
2011-12-17 00:30:44 -0500

    Fixed the treetop grammar to parse sections correctly

--- lib/haproxy/treetop/nodes.rb
+++ lib/haproxy/treetop/nodes.rb
@@ -1,49 +1,87 @@
 module HAProxy::Treetop
-  class Char < Treetop::Runtime::SyntaxNode; end
-  class CommentText < Treetop::Runtime::SyntaxNode; end
-  class LineBreak < Treetop::Runtime::SyntaxNode; end
-  class BlankLine < Treetop::Runtime::SyntaxNode; end
-  class CommentLine < Treetop::Runtime::SyntaxNode; end
-  class UnknownLine < Treetop::Runtime::SyntaxNode; end
-  class ServerLine < Treetop::Runtime::SyntaxNode; end
-  class GlobalSection < Treetop::Runtime::SyntaxNode; end
-  class DefaultsSection < Treetop::Runtime::SyntaxNode; end
+  extend self
 
-  class ConfigurationNode < Treetop::Runtime::SyntaxNode
-  end
-
-  class BackendNode < Treetop::Runtime::SyntaxNode
-    def servers
-      elements.select { |e| e.class == HAProxy::Treetop::ServerNode }
-    end
-  end
-
-  class BlankLineNode < Treetop::Runtime::SyntaxNode; end
-
-  class CommentNode < Treetop::Runtime::SyntaxNode
-    def comment_text
-      elements[1].text_value
-    end
-  end
-
-  class AttributeNode < Treetop::Runtime::SyntaxNode
+  module StrippedTextContent
     def content
       self.text_value.strip
     end
   end
 
-  class ServerNode < Treetop::Runtime::SyntaxNode
+  class Whitespace < Treetop::Runtime::SyntaxNode
+    def content; self.text_value; end
+  end
+  class LineBreak < Treetop::Runtime::SyntaxNode; end
+  class Char < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class Keyword < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class Value < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class CommentText < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class BlankLine < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class ConfigLine < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class CommentLine < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+
+  class ServerLine < Treetop::Runtime::SyntaxNode
+    include StrippedTextContent
     def name
-      elements[2].text_value
+      nil
     end
 
     def address
-      elements[4].text_value
+      nil
+    end
+  end
+
+  class GlobalHeader < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class DefaultsHeader < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class FrontendHeader < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+  class BackendHeader < Treetop::Runtime::SyntaxNode; include StrippedTextContent; end
+
+  class ConfigBlock < Treetop::Runtime::SyntaxNode; end
+
+  class DefaultsSection < Treetop::Runtime::SyntaxNode; end
+  class GlobalSection < Treetop::Runtime::SyntaxNode; end
+  class FrontendSection < Treetop::Runtime::SyntaxNode; end
+
+  class BackendSection < Treetop::Runtime::SyntaxNode
+    def servers
+       self.config_block.elements.select {|e| e.class == ServerLine}
     end
 
-    def params
-      elements[6].elements.select {|e| e.text_value.strip != ''}.map {|e| e.text_value.strip }
+    def config_block
+      self.elements.select {|e| e.class == ConfigBlock}
     end
+  end
+
+  class ConfigurationFile < Treetop::Runtime::SyntaxNode
+    def global
+      self.elements.select {|e| e.class == GlobalSection}.first
+    end
+
+    def defaults
+      self.elements.select {|e| e.class == DefaultsSection}.first
+    end
+       
+    def frontends
+      self.elements.select {|e| e.class == FrontendSection}
+    end
+
+    def backends
+      self.elements.select {|e| e.class == BackendSection}
+    end
+  end
+
+  def print_node(e, depth, options = nil)
+    options ||= {}
+    options = {:max_depth => 2}.merge(options)
+
+    puts if depth == 0
+    print "--" * depth
+    print " #{e.class.name.split('::').last}"
+    print " [#{e.text_value}]" if e.class == Treetop::Runtime::SyntaxNode
+    print " [#{e.content}]" if e.respond_to? :content
+    puts
+    e.elements.each do |child|
+      print_node(child, depth + 1, options)
+    end if depth < options[:max_depth] && e.elements && !e.respond_to?(:content)
   end
 end
 
--- spec/haproxy/treetop/config_parser_spec.rb
+++ spec/haproxy/treetop/config_parser_spec.rb
@@ -6,34 +6,40 @@ require 'haproxy/treetop/config'
 describe 'HAProxy::Treetop::ConfigParser' do
   before(:each) do
     @parser = HAProxy::Treetop::ConfigParser.new
+    @result = @parser.parse(File.read("spec/fixtures/multi-pool.haproxy.cfg"))
+    if @result.nil?
+      puts
+      puts "Failure Reason:  #{@parser.failure_reason}"
+    end
+
+    HAProxy::Treetop.print_node(@result, 0, :max_depth => 3)
   end
 
-  it 'should parse a backend server block' do
-    # puts File.read("spec/fixtures/simple.haproxy.cfg")
-    result = @parser.parse(File.read("spec/fixtures/simple.haproxy.cfg"))
-#    result = @parser.parse %{
-## server web1 66.66.66.66:80 weight 1 maxconn 512 check
-#server web1 66.66.66.66:80 weight 1 maxconn 512 check
-#server web2 66.77.66.66:80 weight 2 maxconn 512 check
-#
-#}
+  it "should parse a backend server block" do
+    pending
+    backend = @result.backends.first
+    backend.servers.size.should == 4
+    backend.servers[0].name.should == 'prd_www_1'
+    backend.servers[0].address.should == '10.214.78.95'
+  end
 
-    if result.nil?
-      puts
-      puts @parser.failure_reason
-    end
-    result.class.name.should == 'HAProxy::Treetop::ConfigurationNode'
+  it 'should parse a file with multiple sections' do
+    @result.class.should == HAProxy::Treetop::ConfigurationFile
+    @result.elements.size.should == 5
 
-    puts
-    result.elements.each do |e|
-      puts "#{e.class.name} #{e.text_value.strip}"
-    end
-    require 'pp'
-    # pp result
-    result.elements.size.should == 4
-    result.servers.size.should == 2
-    result.servers[0].name.should == 'web1'
-    result.servers[1].name.should == 'web2'
+    @result.global.should == @result.elements[0]
+    @result.elements[0].class.should == HAProxy::Treetop::GlobalSection
+
+    @result.defaults.should == @result.elements[1]
+    @result.elements[1].class.should == HAProxy::Treetop::DefaultsSection
+
+    @result.frontends[0].should == @result.elements[2]
+    @result.elements[2].class.should == HAProxy::Treetop::FrontendSection
+
+    @result.backends[0].should == @result.elements[3]
+    @result.backends[1].should == @result.elements[4]
+    @result.elements[3].class.should == HAProxy::Treetop::BackendSection
+    @result.elements[4].class.should == HAProxy::Treetop::BackendSection
   end
 
 end
