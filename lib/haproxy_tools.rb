7fb919529777c426b387ace3d5b26207fe09d595
Jason Wadsworth <jdwadsworth@gmail.com>
2012-01-30 00:37:10 -0500

    Added runtime version constant

--- lib/haproxy_tools.rb
+++ lib/haproxy_tools.rb
@@ -5,3 +5,7 @@ require 'haproxy/config'
 require 'haproxy/renderer'
 require 'haproxy/parser'
 
+module HAProxy
+  VERSION = File.read(File.join(File.dirname(__FILE__), '..', 'VERSION')).strip
+end
+
