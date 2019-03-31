c4914ec5421a3d0b00be2ff5318d9def43b89eda
Jason Wadsworth <jdwadsworth@gmail.com>
2019-03-30 23:35:13 -0400

    Remove config for old-style assertions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  config.expect_with(:rspec) { |c| c.syntax = :should }
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
